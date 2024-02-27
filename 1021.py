ca145547e231c8feabb5e3669efb7e40d7a1a5a2
alfredopaulo <alfredopaulo2003@gmail.com>
2024-02-27 19:07:17 -0400

    olha a pedra

--- 1021.py
+++ 1021.py
@@ -1,8 +1,7 @@
 valor = float(input(""))
 
 notas = [100,50,20,10,5,2]
-moedas = [1.0,0.5,0.25,0.10,0.05,0.01]
-
+moedas = [1.0,0.50,0.25,0.10,0.05,0.01]
 
 def calcular(lista , msg, valor, titulo):
     aux = valor
@@ -13,11 +12,8 @@ def calcular(lista , msg, valor, titulo):
         while aux >= i:
             notas+=1
             aux-=i
-        print(f"{notas} {msg}(s) de R$ {i}{(lista[1]>1) ? ".00" : }")
+        print(f"{notas} {msg}(s) de R$ {i}.00") if lista[1]>1 else print(f"{notas} {msg}(s) de R$ {i}")
     return aux
-
-
-(x % 2 == 0) ? "par" : "impar"
 
 valor = calcular(notas,"nota",valor,"NOTAS")
 calcular(moedas,"moeda", valor, "MOEDAS")
