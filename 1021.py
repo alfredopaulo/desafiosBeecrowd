55d4063f55063c69d191ce81b7d15e89fe0309ce
Alfredo Paulo <alfredopaulo2003@gmail.com>
2024-02-27 23:02:35 -0400

    1021.py

--- 1021.py
+++ 1021.py
@@ -5,15 +5,15 @@ moedas = [1.0,0.50,0.25,0.10,0.05,0.01]
 
 def calcular(lista , msg, valor, titulo):
     aux = valor
-    notas = 0
+    qnotas = 0
     print(f"{titulo}:")
     for i in lista:
-        notas = 0 
-        while aux >= i:
-            notas+=1
+        qnotas = 0 
+        while round(aux,2) >= i:
+            qnotas+=1
             aux-=i
-        print(f"{notas} {msg}(s) de R$ {i}.00") if lista[1]>1 else print(f"{notas} {msg}(s) de R$ {i}")
+        print(f"{qnotas} {msg}(s) de R$ {i}.00") if lista[1]>1 else print(f"{qnotas} {msg}(s) de R$ {i:.2f}")
     return aux
 
 valor = calcular(notas,"nota",valor,"NOTAS")
-calcular(moedas,"moeda", valor, "MOEDAS")
+calcular(moedas,"moeda", round(valor,2), "MOEDAS")
